35e90b144e4dd1c7868176eef4ac934b04e6019d
Thant Ko Zaw <thantmkou10@gmail.com>
2021-09-26 14:01:20 +0630

    Change @board to @grid

--- lib/board.rb
+++ lib/board.rb
@@ -3,18 +3,20 @@ require_relative 'miscellaneous'
 class Board
   include Miscellaneous
 
+  attr_reader :grid
+
   def initialize
-    @board = Array.new(6) { Array.new(7) { empty_circle } }
+    @grid = Array.new(6) { Array.new(7) { empty_circle } }
   end
 
   def display_board
-    @board.each do |row|
+    @grid.each do |row|
       puts row.join(' ')
     end
     puts (1..7).to_a.join(' ')
   end
 
   def update_board(row, column, symbol)
-    @board[row][column] = symbol
+    @grid[row][column] = symbol
   end
 end
