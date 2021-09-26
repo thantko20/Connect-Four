5dcd260d0ef045594023a38f9726b9d3cd6d00a7
Thant Ko Zaw <thantmkou10@gmail.com>
2021-09-27 00:33:11 +0630

    Add rspec tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,18 +1,19 @@
 require_relative '../lib/board'
 
 describe Board do
-  subject(:board_update) { Board.new }
-  let(:yellow_circle) { board_update.yellow_circle }
-  let(:blue_circle) { board_update.blue_circle }
+  let(:yellow_circle) { described_class.new.yellow_circle }
+  let(:blue_circle) { described_class.new.blue_circle }
 
   describe '#update_board' do
+    subject(:board_update) { Board.new }
+
     context 'when updated at 1st row and 1st column with yello circle' do
       before do
         board_update.update_board(1, 1, yellow_circle)
       end
 
       it 'updates cell on the board' do
-        board = board_update.instance_variable_get(:@board)
+        board = board_update.instance_variable_get(:@grid)
         updated_cell = board[1][1]
         expect(updated_cell).to eq(yellow_circle)
       end
@@ -24,9 +25,81 @@ describe Board do
       end
 
       it 'updated cell on the board' do
-        board = board_update.instance_variable_get(:@board)
+        board = board_update.instance_variable_get(:@grid)
         updated_cell = board[5][0]
         expect(updated_cell).to eq(blue_circle)
+      end
+    end
+  end
+
+  describe '#check_horizontal' do
+    subject(:horizontal_check) { described_class.new }
+    let(:grid) { horizontal_check.instance_variable_get(:@grid)}
+
+    before do
+      grid[0][1] = 'X'
+      grid[0][2] = 'X'
+      grid[0][3] = 'X'
+      grid[0][4] = 'X'
+    end
+    context 'when it matches' do
+      it 'returns true' do
+        result = horizontal_check.check_horizontal(0, 1, 'X')
+        expect(result).to be true
+      end
+    end
+  end
+
+  describe '#check_vertical' do
+    subject(:vertical_check) { described_class.new }
+    let(:grid) { vertical_check.instance_variable_get(:@grid)}
+
+    before do
+      grid[0][1] = 'X'
+      grid[1][1] = 'X'
+      grid[2][1] = 'X'
+      grid[3][1] = 'X'
+    end
+    context 'when it matches' do
+      it 'returns true' do
+        result = vertical_check.check_vertical(0, 1, 'X')
+        expect(result).to be true
+      end
+    end
+  end
+
+  describe '#check_right_diagonal' do
+    subject(:rt_diagonal_check) { described_class.new }
+    let(:grid) { rt_diagonal_check.instance_variable_get(:@grid )}
+
+    before do
+      grid[2][6] = 'X'
+      grid[3][5] = 'X'
+      grid[4][4] = 'X'
+      grid[5][3] = 'X'
+    end
+    context 'when it matches' do
+      it 'returns true' do
+        result = rt_diagonal_check.check_right_diagonal(2, 6, 'X')
+        expect(result).to be true
+      end
+    end
+  end
+
+  describe '#check_left_diagonal' do
+    subject(:lt_diagonal_check) { described_class.new }
+    let(:grid) { lt_diagonal_check.instance_variable_get(:@grid )}
+
+    before do
+      grid[0][2] = 'X'
+      grid[1][3] = 'X'
+      grid[2][4] = 'X'
+      grid[3][5] = 'X'
+    end
+    context 'when it matches' do
+      it 'returns true' do
+        result = lt_diagonal_check.check_left_diagonal(0, 2, 'X')
+        expect(result).to be true
       end
     end
   end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,107 @@
-require_relative '..lib/game'
+require_relative '../lib/game'
 
 describe Game do
-  
+  describe '#verify_input' do
+    subject(:input_verification) { described_class.new }
+
+    context 'when player types valid input which is 2' do
+      valid_input = 2
+      it 'returns true' do
+        result = input_verification.verify_input(valid_input)
+        expect(result).to be true
+      end
+    end
+
+    context 'when player types invalid value that is already occupied' do
+      let(:game_board) { input_verification.instance_variable_get(:@board) }
+
+      before do
+        game_board.grid[0][2] = 'X'
+      end
+
+      it 'returns false' do
+        invalid_input = 2
+        result = input_verification.verify_input(invalid_input)
+        expect(result).to be false
+      end
+    end
+
+    context 'when player types invalid value greater than 7' do
+      it 'returns false' do
+        invalid_input = 8
+        result = input_verification.verify_input(invalid_input)
+        expect(result).to be false
+      end
+    end
+
+    context 'when player types invalid value less than 1' do
+      it 'returns false' do
+        invalid_input = -2
+        result = input_verification.verify_input(invalid_input)
+        expect(result).to be false
+      end
+    end
+  end
+
+  describe '#player_turn' do
+    subject(:turn_verification) { described_class.new }
+    let(:player1) { turn_verification.instance_variable_get(:@player1) }
+    let(:player2) { turn_verification.instance_variable_get(:@player2) }
+
+    before do
+      player1.name = 'Marco'
+      player2.name = 'Micah'
+    end
+
+    context "when it is player1's turn" do
+
+      it 'returns player1' do
+        return_player = turn_verification.player_turn
+        expect(return_player.name).to eq(player1.name)
+      end
+    end
+
+    context "when it is player2's turn" do
+
+      before do
+        turn_verification.instance_variable_set(:@turn, 1)
+      end
+
+      it 'returns player2' do
+        return_player = turn_verification.player_turn
+        expect(return_player.name).to eq(player2.name)
+      end
+    end
+  end
+
+  describe '#game_over?' do
+    context 'when a player wins' do
+      subject(:end_game) { described_class.new }
+      let(:board) { end_game.instance_variable_get(:@board) }
+      let(:player) { instance_double(Player, name: 'Marco', symbol: 'X') }
+
+      before do
+        allow(board).to receive(:check_row).and_return(true)
+      end
+
+      it "announces player's name" do
+        expect(end_game).to receive(:puts).with('Marco won!')
+        end_game.game_over?(player)
+      end
+    end
+  end
+
+  describe '#draw?' do
+    subject(:game_draw) { described_class.new }
+
+    context 'when draws' do
+      before do
+        game_draw.instance_variable_set(:@turn, 42)
+      end
+      it 'prints Draw!' do
+        expect(game_draw).to receive(:puts).with('Draw!')
+        game_draw.draw?
+      end
+    end
+  end
 end
