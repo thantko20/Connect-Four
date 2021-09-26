a4836c87c0ea72bf0613d7fb1b4ba77f9f38ec8b
Thant Ko Zaw <thantmkou10@gmail.com>
2021-09-26 14:12:38 +0630

    Add #available_row

--- lib/game.rb
+++ lib/game.rb
@@ -14,6 +14,39 @@ class Game
 
   def play
     #TODO
+    loop do
+      @board.display_board
+      set_player_name
+      column = player_input
+      row = available_row
+      @board.update_board(row, column, symbol)
+    end
+  end
+
+  def player_input
+    loop do
+      puts 'Enter column number: '
+      column = gets.chomp.to_i
+      return column if verify_input(column)
+
+      puts "Invalid input!\n\n"
+    end
+  end
+
+  def verify_input(input)
+    input -= 1
+    input.between?(0, 6) && !full_column?(input)
+  end
+
+  def full_column?(input)
+    @board.grid[0][input] != empty_circle
+  end
+
+  def available_row(column)
+    row = 0
+    loop do
+      return row if row == 6 || @board.grid[row + 1][column] != empty_circle
+    end
   end
 
   private
@@ -28,3 +61,6 @@ class Game
     @player2.name = ask_player_name(2)
   end
 end
+
+game = Game.new
+game.play
